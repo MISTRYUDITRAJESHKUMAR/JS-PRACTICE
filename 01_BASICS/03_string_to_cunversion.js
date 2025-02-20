47d64adae6bac9324530b74ae29fbd69ab0d25e0
UDITMISTRYRAJESHKUMAR <uditmistryrajeshkumar@gmail.com>
2025-02-20 18:04:00 +0000

    03ok

---
+++ 01_BASICS/03_string_to_cunversion.js
@@ -0,0 +1,33 @@
+// console.log("ui");
+
+
+// let score = 'true'
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// // Number() is use for a cunvert 100%  to number
+// let valueinnumber = Number(score)
+// console.log(typeof valueinnumber);
+// console.log(valueinnumber);
+
+
+//'33'   33
+// '33'   NaN
+// true  1
+// false 0
+
+
+// let isloggdin = 0
+
+// let boolean = Boolean(isloggdin)
+// console.log(boolean);
+
+// let num = 19;
+
+// let oknum = String(num)
+// console.log(oknum);
+// console.log(typeof oknum);
+
+
+
