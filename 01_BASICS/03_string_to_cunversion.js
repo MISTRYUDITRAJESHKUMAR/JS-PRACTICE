14e6f2671068ee42b1327e23e1f4ea0c08e66d3e
UDITMISTRYRAJESHKUMAR <uditmistryrajeshkumar@gmail.com>
2025-02-20 19:04:40 +0000

    ok push

--- 01_BASICS/03_string_to_cunversion.js
+++ 01_BASICS/03_string_to_cunversion.js
@@ -31,3 +31,99 @@
 
 
 
+// let a = '10.23'
+// let b = parseInt(a)
+// let c = parseFloat(a)
+
+
+// console.log(b);
+// console.log(c);
+
+
+
+// let str = "udit"
+// let numberc = Number(str)
+// console.log(numberc);
+// console.log(typeof NaN);
+
+
+
+
+// let nameu = '8udit5'
+// let nameb = parseInt(nameu)
+// console.log(nameb);
+
+
+
+// let a = 150.667;
+// let b = parseInt(a)
+// let c = parseFloat(a)
+
+
+// console.log(b);
+// console.log(c);
+
+
+
+
+// let a = '92'
+// let b = Number(a)
+// if (isNaN(b)) {
+//     console.log("it is not a number");
+// }
+// else {
+//     console.log("it is  a number");
+
+// }
+
+// let val = 0
+// let relval = Boolean(val)
+// if (Boolean(val)) {
+//     console.log('this is true ');
+// } 
+// else {
+//     console.log('this is false ');
+// }
+
+
+// let a = ''
+// let cunvernum = Number(a)
+
+
+
+
+// if ( a == '') {
+//     console.log("this outpu is o");
+// }
+// else 
+// {
+//     console.log("here added a number");
+// }
+
+
+
+
+// let str = "   121  "
+// let pars = parseInt(str)
+// console.log(pars);
+
+
+// let float = '611.121.1.1';
+// let change = parseFloat(float)
+// console.log(change);
+
+
+
+
+// let namea = '0'
+// let change = parseInt(namea)
+// if (isNaN(change)) {
+//     console.log("this us not a int");
+// } 
+// else if (namea == 0 ){
+//     console.log("this is zero");
+
+// }
+// else {
+//     console.log("this is a int");
+// }
