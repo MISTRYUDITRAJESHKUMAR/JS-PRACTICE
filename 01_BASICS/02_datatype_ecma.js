bd165e7cc425e3d4970c8e2611af09e2dd62171e
UDITMISTRYRAJESHKUMAR <uditmistryrajeshkumar@gmail.com>
2025-02-20 16:08:03 +0000

    ok

---
+++ 01_BASICS/02_datatype_ecma.js
@@ -0,0 +1,16 @@
+"use strict";
+
+
+//alert(3+2) we are using node 
+
+let name = "udit"
+let age = 17
+let loggin = false
+let uddi;
+let udd = '';
+
+// null
+// Symbol
+// Object
+
+console.log(typeof undefined);
