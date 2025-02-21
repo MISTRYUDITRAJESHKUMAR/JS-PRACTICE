19016273a1f70affa5d508f4d34c6971116749f3
UDITMISTRYRAJESHKUMAR <uditmistryrajeshkumar@gmail.com>
2025-02-21 19:32:14 +0000

    ok 6 final

--- 01_BASICS/06_strings.js
+++ 01_BASICS/06_strings.js
@@ -8,7 +8,7 @@
 
 
 
-const game = new String("cricket");
+// const game = new String("cricket");
 // console.log(game[0]);
 // console.log(game.__proto__)    
 
@@ -20,24 +20,130 @@ const game = new String("cricket");
 // console.log(game.indexOf('c'));
 
 
-const newstring = game.substring(0,2)
-console.log(newstring);
+// const newstring = game.substring(0,2)
+// console.log(newstring);
 
 
-const another = game.slice(0,3)
-console.log(another);
+// const another = game.slice(0,3)
+// console.log(another);
 
-const news = "   udit    "
-console.log(news);
-console.log(news.trim());
+// const news = "   udit    "
+// console.log(news);
+// console.log(news.trim());
 
-const url = "https://www.google.%20.com"
-console.log(url.replace("%20", "boby"));
-console.log(url.includes('%20'));
-
-
-const uditname = "udit-mistry";
-console.log(uditname.split('-'));
-console.log(typeof split);
+// const url = "https://www.google.%20.com"
+// console.log(url.replace("%20", "boby"));
+// console.log(url.includes('%20'));
 
 
+// const uditname = "udit-mistry";
+// console.log(uditname.split('-'));
+// console.log(typeof split);
+
+
+// function employee  (name, id, add){
+//    this.name = name;
+//    this.id = id;
+//    this.add = add;
+// }
+// employee.prototype.salary = 40000;
+
+// const em1 = new employee("udit",1,"sectorr24")
+// const em2= new employee("uditt",2,"sector24")
+// console.log(em1);
+// console.log(em2.name)
+// let sub = "udit"
+// let subb = sub.substring(1,3);
+// console.log(subb);
+
+// const udit = "boyey"
+
+// const slice = udit.slice(2,3)
+// console.log(slice);
+
+
+// const udit = "name"
+// const realname = udit.replace('name','mistry')
+// console.log(realname);
+
+// let naame = "mistry udit rrajeshkumar"
+
+// console.log( relname = naame.includes('rajeshkumar'));
+
+
+// questionsss+++++++++++++++++++++++++++++++++++++++++++
+
+// 1
+
+// How do you concatenate two strings in JavaScript? 
+
+// let a = "udit"
+// let b = "  mistry"
+
+// let c = a+b
+// console.log(c);
+
+
+// let Name = 'udit'
+// let age = 18
+
+// console.log(`the boy name is ${Name} and him age is ${age}`
+// );
+
+
+
+// 2
+// Q2: How do you convert a string to uppercase in JavaScript?
+
+
+// let udit = "mistry udit"
+// console.log(udit.toUpperCase());
+
+
+// Q3: How do you remove whitespace from both ends of a string in JavaScript?
+// let  full = "     udit          "
+// console.log(full.trim());
+
+
+// Q4: How do you replace a substring within a string in JavaScript?
+// const udit = 'boy'
+// console.log(udit.replace('boy','male'));
+
+
+// Q5: How do you check if a string contains a specific substring in JavaScript?
+
+// let Name = 'udit mistry'
+// console.log(Name.includes('udit'));
+
+
+// Q6: How do you extract a part of a string using the slice() method in JavaScript?
+
+// let udit = 'misitry udit'
+// console.log(udit.slice(3,4));
+
+
+// Q7:How do you split a string into an array of substrings in JavaScript?
+
+// let udit = 'udit===mistry'
+// console.log(udit.split('='));
+
+
+//Q8: How do you find the index of the first occurrence of a character in a string in JavaScript?
+
+
+// let uidt = "name"
+// console.log(uidt.indexOf('n'));
+
+
+// Q9: How do you extract a substring from a string using the substring() method in JavaScript?
+
+
+// let udit = 'uditmistry'
+// console.log(udit.substring(0,4));
+
+
+// Q10: How do you check if a string starts with a specific substring in JavaScript?
+
+// 
+// let a =  'mistryudit'
+// console.log(a.startsWith('mistry'));
