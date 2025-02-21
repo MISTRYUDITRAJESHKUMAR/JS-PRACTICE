7494fc4c0f5293773ec0de8ba0982038cc357efe
UDITMISTRYRAJESHKUMAR <uditmistryrajeshkumar@gmail.com>
2025-02-21 16:09:26 +0000

    ok update

--- 01_BASICS/05_datatype_summery.js
+++ 01_BASICS/05_datatype_summery.js
@@ -11,21 +11,21 @@
 
 
 
-const a = 1
-const b = 18.77
+// const a = 1
+// const b = 18.77
 
-const isin = false
-const temp = null
-let userEmail;
+// const isin = false
+// const temp = null
+// let userEmail;
 
-const id = Symbol('123')
-const anotther = Symbol('123')
+// const id = Symbol('123')
+// const anotther = Symbol('123')
 
-console.log(id === anotther);
+// console.log(id === anotther);
 
 
 
-const bigN = 66267267726n
+// const bigN = 66267267726n
 // refrence(non premittive)            
 
 // obj
@@ -46,3 +46,35 @@ const bigN = 66267267726n
 // }
 // uditt()
 // console.log(typeof function);
+
+
+
+
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// stack(premitive),heap(non premitive)
+
+
+let myName = "udit"
+
+let newname = myName
+newname = "uditmistry"
+
+console.log(myName);
+console.log(newname);
+
+
+
+let user = {
+    email:"udit@",
+    upi:"udigsa"
+}
+
+
+let user2 = user
+
+user2.email = "nana"
+console.log(user.email);
+console.log(user2.email);
