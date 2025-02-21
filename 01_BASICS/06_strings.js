11efe6cc56ff8a0fe199e3d715752bc0894ebf7d
UDITMISTRYRAJESHKUMAR <uditmistryrajeshkumar@gmail.com>
2025-02-21 17:35:41 +0000

    done 6

---
+++ 01_BASICS/06_strings.js
@@ -0,0 +1,43 @@
+// const Name = "udit"
+// const id =   "   mistry"
+
+// console.log(Name + id);
+
+
+// console.log(`hello my name is ${Name} and my id is ${id}`);
+
+
+
+const game = new String("cricket");
+// console.log(game[0]);
+// console.log(game.__proto__)    
+
+
+// console.log(game.length);
+// console.log(game.toUpperCase());
+// console.log(game.toLowerCase());
+// console.log(game.charAt(5));
+// console.log(game.indexOf('c'));
+
+
+const newstring = game.substring(0,2)
+console.log(newstring);
+
+
+const another = game.slice(0,3)
+console.log(another);
+
+const news = "   udit    "
+console.log(news);
+console.log(news.trim());
+
+const url = "https://www.google.%20.com"
+console.log(url.replace("%20", "boby"));
+console.log(url.includes('%20'));
+
+
+const uditname = "udit-mistry";
+console.log(uditname.split('-'));
+console.log(typeof split);
+
+
