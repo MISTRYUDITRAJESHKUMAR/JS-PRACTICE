46de6e027f897534a64129aed7c9e045c0275f3c
UDITMISTRYRAJESHKUMAR <uditmistryrajeshkumar@gmail.com>
2025-02-21 04:54:01 +0000

    ok5

---
+++ 01_BASICS/05_datatype_summery.js
@@ -0,0 +1,48 @@
+//prremittivve 
+
+// 7)
+// string
+// number
+// bollean
+// null
+// undifined
+// symbol
+// big int
+
+
+
+const a = 1
+const b = 18.77
+
+const isin = false
+const temp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotther = Symbol('123')
+
+console.log(id === anotther);
+
+
+
+const bigN = 66267267726n
+// refrence(non premittive)            
+
+// obj
+// array
+//funcion
+
+
+
+// const heros = ['udit','mistry']
+
+// let udit = {
+//     name:'udit',
+//     id :'12'
+// }
+
+// const uditt = function () {
+//     console.log("udit");
+// }
+// uditt()
+// console.log(typeof function);
