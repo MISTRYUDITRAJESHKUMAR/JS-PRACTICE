e760d7508916bb9a3fcaff71ed235c714c4954fb
UDITMISTRYRAJESHKUMAR <uditmistryrajeshkumar@gmail.com>
2025-02-23 19:30:56 +0000

    okk 8

---
+++ 01_BASICS/08_date_and_time.js
@@ -0,0 +1,38 @@
+
+// let mydate = new Date()
+// console.log(mydate.toString());
+// console.log(mydate.toDateString());
+// console.log(mydate.toUTCString());
+// console.log(mydate.toLocaleString());
+
+// console.log(typeof mydate);
+
+
+// let myDate = new Date(2006 , 8 ,14 )
+// console.log(myDate.toDateString());
+
+
+// let withtime = new Date(2006 , 8, 14, 14 , 30 , 55 ,)
+// console.log(withtime.toLocaleString());
+
+
+// let formate = new Date("2006-09-14")
+// console.log(formate.toLocaleString());
+
+// let datee = new Date('2021-09-14')
+// let date = Date.now()
+// // console.log(date);
+// // console.log(datee.getTime());
+// // console.log(Date.now()/1000);
+// // console.log(Math.floor(Date.now()/1000));
+// let naw = new Date(2006, 8, 14)
+// console.log(naw.getMonth() + 1);
+// naw.toLocaleString('default',{
+//     weekday:"long",
+//     timeZone:'Asia/Dhaka'
+// })
+// console.log(naw);
+
+
+
+
