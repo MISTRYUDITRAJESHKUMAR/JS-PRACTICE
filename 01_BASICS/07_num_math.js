87a1fa8d5311291432897e00579e3be052b09b07
UDITMISTRYRAJESHKUMAR <uditmistryrajeshkumar@gmail.com>
2025-02-23 16:30:38 +0000

    ok update 6

---
+++ 01_BASICS/07_num_math.js
@@ -0,0 +1,110 @@
+// const score = 200
+// // console.log(score);
+
+
+// const balance = new Number(151.1551355454544553)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+
+// // to fix is used to fix the decimal points
+// console.log(balance.toFixed(2));
+
+
+// const num = 2231.325225
+// console.log(num.toPrecision(7))
+
+
+
+// let hundred = 100000
+// console.log(hundred.toLocaleString('en-IN'));
+
+
+// Math Object=+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// const num = -100
+// const arr = [5,7,89,4,7,4,88,57,77,99]
+// console.log(Math.PI);
+// console.log(Math.abs(num));
+// console.log(Math.round(4.5));
+// console.log(Math.ceil(4.2));
+// // if floor = before desimal the value are giving bef ore desimal
+// console.log(Math.floor(4.7));
+// console.log(Math.min(5,7,89,4,7,4,88,57,77,99)); 
+// console.log(Math.max(5,7,89,4,7,4,88,57,77,99));
+
+// console.log(Math.random());
+// console.log(Math.floor(Math.random() * 10) + 1);
+
+// const min = 1
+// const max = 6
+// console.log(Math.floor((Math.random() * (max - min + 1)+ min))
+
+// const min = 0
+// const max = 1
+// console.log(Math.floor(Math.random() * (max - min +1 )+ min));
+
+
+
+
+
+// // questions Object=+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// How do you convert a number to a string in JavaScript?
+
+
+// let num = 123
+// let or = num.toString()
+// console.log(or);
+
+
+
+// How do you create a Number object in JavaScript?
+
+// const bal = new Number(123.15)
+// console.log(bal);
+// console.log(typeof bal);
+
+
+
+// const udit = new String('udit','misra')
+// console.log(udit);
+// console.log(typeof udit);
+
+
+// How do you get the length of a number when converted to a string?
+
+
+// let num = new Number(1515.24687)
+// let string = num.toString()
+// console.log(string.length);
+
+
+// How do you fix the number of decimal places of a number?
+
+// let nume = new Number(1515.24687)
+// console.log(nume.toFixed(2));
+
+// How do you format a number to a specific precision?
+
+// let udit = new Number(1515.24687)
+// console.log(udit.toPrecision(5));
+
+
+// let str = 1012500
+// console.log(str.toLocaleString('en-IN'));
+
+
+// console.log(Math.floor(Math.PI));
+
+
+// let a = -3213135
+// console.log(Math.abs(a));
+
+
+
+// let a = 2.5
+// console.log(Math.round(a));
+
+// let a = 2.1
+// console.log(Math.ceil(a));
