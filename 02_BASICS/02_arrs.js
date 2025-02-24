4c126b02a57bf7db3939c1cfc6390efd5da0f544
MISTRY UDIT RAJESHKUMAR <MISTRYUDIT10@GMAIL.COM>
2025-02-24 22:48:36 +0530

    Add files via upload

---
+++ 02_BASICS/02_arrs.js
@@ -0,0 +1,32 @@
+const marvel_hero = ['thor', 'ironman', 'spiderman']
+const dc = ['batman', 'superman', 'wonderwoman']
+
+// marvel_hero.push(dc)
+// console.log(marvel_hero);
+// console.log(marvel_hero[3][0]);
+
+// const newhero = marvel_hero.concat(dc)
+// console.log(newhero);
+
+
+// const add = [...marvel_hero, ...dc]
+// console.log(add);
+
+
+// const add1 = [1, 2, 3, [4, 5, 6], 7,[8, 9, 10]]
+// const flating = add1.flat(Infinity)
+// console.log(flating);
+
+
+console.log(udit);
+
+console.log(Array.isArray(marvel_hero));
+const udit = console.log(Array.from('udit'));
+console.log(Array.isArray(udit));
+
+
+let score = 100
+let score1 = 200
+let score2 = 300
+console.log(Array.of(score, score1, score2));
+
