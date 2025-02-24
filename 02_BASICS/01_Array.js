9735c8a2c1c8e758a284c8fdf33aed2718fe715f
MISTRY UDIT RAJESHKUMAR <MISTRYUDIT10@GMAIL.COM>
2025-02-24 16:39:40 +0000

    01_arr

---
+++ 02_BASICS/01_Array.js
@@ -0,0 +1,63 @@
+//array
+
+
+// const myarr2 = new Array(1, 2, 3, 4, 5, 6)
+// console.log(myarr2[0]);
+// myarr.push(10)
+// myarr.pop()
+// myarr.unshift(20)
+// myarr.shift()
+// const myarr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// console.log(myarr[0]);
+// // console.log(myarr)
+// console.log(myarr.includes(5));
+// console.log(myarr.indexOf(3));
+// console.log(myarr.splice(1, 2));
+
+// console.log(typeof newarr);
+
+// console.log(newarr);
+
+
+//slice and splice
+// const newarr = myarr.join()
+// console.log(myarr);
+// const myarr = [1, 2, 3, 4, 5]
+
+// console.log('A', myarr);
+// const myarris = myarr.slice(1, 3)
+// console.log(myarris);
+// console.log('B', myarr);
+
+// const myarr3 = myarr.splice(1, 3)
+// console.log('C', myarr);
+// console.log(myarr3);
+
+
+// How do you access elements in an array??
+
+// const my = [1, 2, 3]
+
+// // console.log(my[0]);
+// // How do you add elements to an array?
+// my.push(5)
+// my.splice(5, 0, 20)
+// my.splice(6, 0, 21)
+// console.log(my);
+
+
+// const a = [1, 2, 3]
+// console.log(a.indexOf(2));
+
+
+// How do you join all elements of an array into a string?
+
+// const a = [1, 2, 3]
+// const joined = a.join()
+// console.log(joined);
+
+// console.log(Array.isArray(a));
+
+
+
+
---
+++ 02_BASICS/tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+r = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// console.log(myarr[0]);
+// // console.log(myarr)
