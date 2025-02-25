e5b61993d7f0cb2739bd50634c8b3c618841307f
MISTRY UDIT RAJESHKUMAR <MISTRYUDIT10@GMAIL.COM>
2025-02-25 11:28:38 +0000

    03_obj

--- 02_BASICS/03_obj.js
+++ 02_BASICS/03_obj.js
@@ -1,1 +1,40 @@
 console.log("udit");
+// singleton
+
+// object litrrals
+
+const mySym = Symbol("key1")
+
+const jsUser = {
+    name: 'Hitesh',
+    [mySym] : 'ok',
+    "full name" : 'mistryudit',
+    age: 18,
+    location:'jaypur',
+    email:'udit@gmail.com',
+    isLoggedIn:false,
+    lastLoginDays:['monday','sunday']
+
+}
+
+// console.log(jsUser.name);
+// console.log(jsUser["name"]);
+// console.log(jsUser["full name"]);
+// console.log(jsUser[mySym]);
+
+
+// jsUser.email = 'bhi@gmail.com'
+// Object.freeze(jsUser)
+// jsUser.email = 'bhihhsh@gmail.com'
+// console.log(jsUser);
+
+
+jsUser.greeting = function () {
+    console.log("udit");
+}
+
+jsUser.greetingtwo = function () {
+    console.log(`my name is ,${this.name}`);
+}
+console.log(jsUser.greeting());
+console.log(jsUser.greetingtwo());
