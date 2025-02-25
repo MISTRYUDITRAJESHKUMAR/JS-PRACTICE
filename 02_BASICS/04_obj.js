2fb314a75dfd13b5d690e0f2a08f0fe9566ff728
MISTRY UDIT RAJESHKUMAR <MISTRYUDIT10@GMAIL.COM>
2025-02-25 15:02:16 +0000

    ok04

---
+++ 02_BASICS/04_obj.js
@@ -0,0 +1,32 @@
+// const tinderUser = new Object()
+
+// const tinderUser = {}
+// tinderUser.id = '123abc'
+// tinderUser.name = 'udit'
+// tinderUser.isLoggedIn = false
+// console.log(tinderUser);
+
+
+const regulerus = {
+    email: 'udud@gmail',
+    fullname: {
+        userFullname: {
+            fName: "udit",
+            lName: "mistry"
+        }
+    }
+}
+// console.log(regulerus.fullname.userFullname.fName);
+const obj1 = {
+    1: 'a',
+    2: 'b',
+}
+const obj2 = {
+    3: 'c',
+    4: 'd',
+}
+
+// const obj3 = { obj1, obj2 }
+const obj3 = Object.assign()
+console.log(obj3);
+
