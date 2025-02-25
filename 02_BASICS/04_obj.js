6b9691a5fc7ffd17da77c4a423a5a5b4b77b6126
MISTRY UDIT RAJESHKUMAR <MISTRYUDIT10@GMAIL.COM>
2025-02-25 17:58:25 +0000

    ok04

--- 02_BASICS/04_obj.js
+++ 02_BASICS/04_obj.js
@@ -1,21 +1,21 @@
 // const tinderUser = new Object()
 
-// const tinderUser = {}
-// tinderUser.id = '123abc'
-// tinderUser.name = 'udit'
-// tinderUser.isLoggedIn = false
+const tinderUser = {}
+tinderUser.id = '123abc'
+tinderUser.name = 'udit'
+tinderUser.isLoggedIn = false
 // console.log(tinderUser);
 
 
-const regulerus = {
-    email: 'udud@gmail',
-    fullname: {
-        userFullname: {
-            fName: "udit",
-            lName: "mistry"
-        }
-    }
-}
+// const regulerus = {
+//     email: 'udud@gmail',
+//     fullname: {
+//         userFullname: {
+//             fName: "udit",
+//             lName: "mistry"
+//         }
+//     }
+// }
 // console.log(regulerus.fullname.userFullname.fName);
 const obj1 = {
     1: 'a',
@@ -25,8 +25,37 @@ const obj2 = {
     3: 'c',
     4: 'd',
 }
+const obj4 = {
+    5: 'e',
+    6: 'f'
+}
 
 // const obj3 = { obj1, obj2 }
-const obj3 = Object.assign()
-console.log(obj3);
+// const obj3 = Object.assign({}, obj1, obj2, obj4)
+
+// const obj3 = {...obj1,...obj2}
+// console.log(obj3);
+
+
+const users = [
+    {
+        id : 1,
+        Email:'udit@gmail'
+    },
+    {
+        id : 1,
+        Email:'udit@gmail'
+    },
+]
+
+users[1].Email
+console.log(tinderUser);
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+
+console.log(tinderUser.hasOwnProperty('name'));
+
+
 
