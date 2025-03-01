daff3a586c866c550ee42fdfe3e65dc94903a185
MISTRY UDIT RAJESHKUMAR <MISTRYUDIT10@GMAIL.COM>
2025-03-01 13:17:34 +0000

    02 done

--- 03_BASICS/02_scope.js
+++ 03_BASICS/02_scope.js
@@ -1,23 +1,59 @@
 // var  c = 300
-let a = 300
+// let a = 300
 
-if (true) {
-    let a = 10
-    const b = 20;
-    console.log("INNER :",a);
-    
-}
+// if (true) {
+//     let a = 10
+//     const b = 20;
+//     console.log("INNER :", a);
 
-for (let i = 0; i < array.length; i++) {
-    const element = array[i];
-    
-}
+// }
 
+// for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
 
+// }
 
 
 
-console.log(a); // 
+
+
+// console.log(a); // 
 // console.log(b); // 10 20 30
 // console.log(c); // 10 20 30
 
+
+function one() {
+    const username = 'udit'
+    function two() {
+        const website = 'youtube'
+        console.log(username);
+    }
+    // console.log(website);
+    two()
+}
+// one()
+
+if (true) {
+    const username = 'udit'
+    if (username === "udit") {
+        const web = "youtube"
+        // console.log(username + web);
+
+    }
+    // console.log(web);
+}
+// console.log(username);
+
+// ++++intresting++++
+
+
+console.log(addone(1));
+addone(1)
+function addone(number) {
+    return number + 1
+}
+
+addtwo(9)
+const addtwo = function (number) {
+    return number + 2
+}
