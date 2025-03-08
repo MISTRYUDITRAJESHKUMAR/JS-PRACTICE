38084729e0ac9a769d5992e542e218a82c57ea73
MISTRY UDIT RAJESHKUMAR <MISTRYUDIT10@GMAIL.COM>
2025-03-08 08:52:39 +0000

    new

--- 03_BASICS/02_scope.js
+++ 03_BASICS/02_scope.js
@@ -22,38 +22,96 @@
 // console.log(c); // 10 20 30
 
 
-function one() {
-    const username = 'udit'
-    function two() {
-        const website = 'youtube'
-        console.log(username);
-    }
-    // console.log(website);
-    two()
-}
+// function one() {
+//     const username = 'udit'
+//     function two() {
+//         const website = 'youtube'
+//         console.log(username);
+//     }
+//     // console.log(website);
+//     two()
+// }
 // one()
 
-if (true) {
-    const username = 'udit'
-    if (username === "udit") {
-        const web = "youtube"
-        // console.log(username + web);
 
-    }
-    // console.log(web);
-}
+
+// function first(){
+//     const username = 'udit'
+//     function second(){
+//         const Name = 'uditya'
+//         console.log(Name);
+//     }
+//     console.log(username);
+//     second()
+
+// }
+// first()
+
+// if (true) {
+//     const username = 'udit'
+//     if (username === "udit") {
+//         const web = "youtube"
+//         // console.log(username + web);
+
+//     }
+//     console.log(web);
+// }
 // console.log(username);
 
-// ++++intresting++++
 
 
-console.log(addone(1));
-addone(1)
-function addone(number) {
-    return number + 1
+// console.log(addone(1));
+// addone(1)
+// function addone(number) {
+//     return number + 1
+// }
+
+// addtwo(9)
+// const addtwo = function (number) {
+//     return number + 2
+// }
+
+// i  = 10
+// for (let i = 0; i < array.length; i++) {
+//     console.log(i);
+
+// }
+
+// function rollo() {
+//     const rollone = 'piya'
+
+//     function rollt() {
+//         const rolltwo = 'udit'
+//         console.log(rollone);
+//         console.log(rolltwo);
+//     }
+//     rollt()
+    
+// }
+// rollo()
+
+
+
+// if(true){
+//     const username = 'uddit'
+//     if(username === 'uddit'){
+//         const website = 'youtube'
+//         console.log(username);
+//         console.log(website);
+//     }
+// }
+
+
+// ++++intresting++++ 
+console.log(addone(2));
+function addone(num) {
+    return num + 1
+    // console.log(num);
 }
 
-addtwo(9)
-const addtwo = function (number) {
-    return number + 2
+
+console.log(addtwo(655));
+const addtwo = function (num) {
+    return num + 2
+    // console.log(num);
 }
