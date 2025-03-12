990f87bf6dcc9245e203feba44805f36cf4f9e33
MISTRY UDIT RAJESHKUMAR <MISTRYUDIT10@GMAIL.COM>
2025-03-12 18:37:36 +0000

    new

---
+++ 03_BASICS/04_iife.js
@@ -0,0 +1,30 @@
+//immediately Invoked Function Exprestion
+
+(function database() {
+    //named iife
+    console.log(`udiy`);
+})();
+
+(function udit() {
+    console.log(`uidt`);
+})();
+
+(() => {
+    console.log(`Db connected`);
+})();
+
+(function udt(){
+
+    console.log('udit');
+    
+})();
+( (name)=>{
+    console.log(`my name is ${name}`);
+    
+})('uddit')
+
+
+
+(function number (number) {
+    console.log(`my number is ${number}`);
+})()
