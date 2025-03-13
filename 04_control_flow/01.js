eb526cd9e71e86d524eee107c6e812f29c87f21c
MISTRY UDIT RAJESHKUMAR <MISTRYUDIT10@GMAIL.COM>
2025-03-13 18:51:11 +0000

    add control flow examples using if statements and switch cases

---
+++ 04_control_flow/01.js
@@ -0,0 +1,51 @@
+//if statement
+
+// const isUserLoggedin = true
+// const tamp = 50
+
+// if (tamp < 50) {
+//     console.log("less then 50");
+// }
+// else {
+//     console.log("more than 50");
+// }
+// <,>,<=,>=,==,!=,=== ,!==
+
+
+// const score = 200
+
+// if (score > 100) {
+//     const power = 'fly'
+//     console.log(`User pawor : ${power}`);
+// }
+// console.log(`User pawor : ${power}`);
+
+
+// const bal = 1000
+// if (bal>500) console.log('udist');
+
+// if (bal < 500) {
+//     console.log('less than');
+// }else if (bal<750){
+//     console.log('less tha n 750'); 
+// }
+// else {
+//     console.log('less tha n 1200'); 
+// }
+
+
+
+const userLogedin = true
+const debit = true
+const loggedingoogle = false
+const loggedinmail = true
+
+
+if (userLogedin && debit && 2=='2') {
+    console.log("allow");
+    
+}
+if (loggedingoogle||loggedinmail) {
+    console.log("allow");
+    
+}
---
+++ 04_control_flow/switch.js
@@ -0,0 +1,29 @@
+// switch (key) {
+//     case value:
+
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+const month = "april"
+switch (month) {
+    case 'jan':
+        console.log('jan');
+        break;
+
+    case "feb":
+        console.log('feb');
+        break;
+
+    case "april":
+        console.log('mach');
+
+        break;
+    default:
+        console.log("april");
+
+        break;
+}
