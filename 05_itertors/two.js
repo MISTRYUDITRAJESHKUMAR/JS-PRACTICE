03fad2f7b5b0adaeeae556eacc580daf86836857
MISTRY UDIT RAJESHKUMAR <MISTRYUDIT10@GMAIL.COM>
2025-03-15 08:39:49 +0000

    js

---
+++ 05_itertors/two.js
@@ -0,0 +1,25 @@
+
+// let i = 0
+// while (i <= 10) {
+//     console.log(`value of index is ${i}`);
+//     i = i + 2;
+// }
+
+
+// const myarr = ['1', '2', '3']
+// let i = 0;
+// while (i < myarr.length) {
+//     console.log(`value of index is ${myarr[i]}`);
+//     i++;
+// }
+
+
+// let name = ['udit','jay','om']
+// let score = 0
+// do {
+//    console.log(`score is score${name[score]}`);
+//     score++;
+// } while (score < name.length);
+
+
+
